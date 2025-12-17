75b8e3b975e363d41195674f853b7fc2909f2951
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-17 14:18:42 +0000

    Add utility function to remove duplicates from a list while preserving order

---
+++ whisk_csidh/data_cleaner.py
@@ -0,0 +1,9 @@
+
+def deduplicate_list(original_list):
+    seen = set()
+    deduplicated = []
+    for item in original_list:
+        if item not in seen:
+            seen.add(item)
+            deduplicated.append(item)
+    return deduplicated
