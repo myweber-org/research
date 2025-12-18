b0bac83d3b27e72d485edc35ba5c77f7ea74b5ba
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-18 19:27:09 +0000

    Add utility function to remove duplicate entries from a list while preserving order

---
+++ papers/casper-basics/data_cleaner.py
@@ -0,0 +1,9 @@
+
+def remove_duplicates(seq):
+    seen = set()
+    result = []
+    for item in seq:
+        if item not in seen:
+            seen.add(item)
+            result.append(item)
+    return result
