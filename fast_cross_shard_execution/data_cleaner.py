5258969438ab509d87168e816ed64b1660fa4718
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-21 20:58:56 +0000

    Add a utility function to remove duplicate entries from a list while preserving order

---
+++ fast_cross_shard_execution/data_cleaner.py
@@ -0,0 +1,9 @@
+
+def remove_duplicates_preserve_order(input_list):
+    seen = set()
+    result = []
+    for item in input_list:
+        if item not in seen:
+            seen.add(item)
+            result.append(item)
+    return result
