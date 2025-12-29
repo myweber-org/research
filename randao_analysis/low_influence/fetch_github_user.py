91191273a33dbc5e969cc101576cdf3898d46ff1
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-29 21:00:16 +0000

    Add script to fetch GitHub user details using the GitHub API

---
+++ randao_analysis/low_influence/fetch_github_user.py
@@ -0,0 +1,35 @@
+import requests
+
+def fetch_github_user(username):
+    """
+    Fetch public details of a GitHub user.
+    """
+    url = f"https://api.github.com/users/{username}"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        user_data = response.json()
+        return {
+            'login': user_data.get('login'),
+            'name': user_data.get('name'),
+            'public_repos': user_data.get('public_repos'),
+            'followers': user_data.get('followers'),
+            'following': user_data.get('following'),
+            'created_at': user_data.get('created_at')
+        }
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        print(f"An error occurred: {err}")
+    return None
+
+if __name__ == "__main__":
+    username = input("Enter GitHub username: ")
+    user_info = fetch_github_user(username)
+    if user_info:
+        print(f"User: {user_info['login']}")
+        print(f"Name: {user_info['name']}")
+        print(f"Public Repos: {user_info['public_repos']}")
+        print(f"Followers: {user_info['followers']}")
+        print(f"Following: {user_info['following']}")
+        print(f"Account Created: {user_info['created_at']}")
