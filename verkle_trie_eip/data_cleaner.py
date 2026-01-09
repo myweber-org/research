eae61d395b12a3fcddbfd2d11305adaf58bcf088
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-09 23:31:53 +0000

    Add data cleaning utility to remove duplicate entries from a list

--- verkle_trie_eip/data_cleaner.py
+++ verkle_trie_eip/data_cleaner.py
@@ -176,4 +176,30 @@ def clean_dataset(data, columns_to_clean):
         if column in cleaned_data.columns:
             cleaned_data = remove_outliers_iqr(cleaned_data, column)
     
-    return cleaned_data
+    return cleaned_datadef remove_duplicates(input_list):
+    seen = set()
+    result = []
+    for item in input_list:
+        if item not in seen:
+            seen.add(item)
+            result.append(item)
+    return result
+
+def clean_data_with_order(input_list, key=None):
+    if key is None:
+        key = lambda x: x
+    seen = set()
+    result = []
+    for item in input_list:
+        identifier = key(item)
+        if identifier not in seen:
+            seen.add(identifier)
+            result.append(item)
+    return result
+
+if __name__ == "__main__":
+    sample = [1, 2, 2, 3, 4, 4, 5]
+    print(remove_duplicates(sample))
+    
+    sample_dicts = [{"id": 1}, {"id": 2}, {"id": 1}, {"id": 3}]
+    print(clean_data_with_order(sample_dicts, key=lambda x: x["id"]))
