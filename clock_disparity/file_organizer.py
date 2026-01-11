b2c15985829aeebd0650ff938361cac95ccd4d48
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-11 22:09:19 +0000

    Add a script to organize files by extension into categorized folders

---
+++ clock_disparity/file_organizer.py
@@ -0,0 +1,28 @@
+
+import os
+import shutil
+
+def organize_files(directory):
+    if not os.path.isdir(directory):
+        print(f"Error: Directory '{directory}' does not exist.")
+        return
+
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        if os.path.isfile(file_path):
+            _, extension = os.path.splitext(filename)
+            extension = extension.lower()
+            if extension:
+                folder_name = extension[1:] + "_files"
+            else:
+                folder_name = "no_extension_files"
+
+            target_folder = os.path.join(directory, folder_name)
+            os.makedirs(target_folder, exist_ok=True)
+            target_path = os.path.join(target_folder, filename)
+            shutil.move(file_path, target_path)
+            print(f"Moved: {filename} -> {folder_name}/")
+
+if __name__ == "__main__":
+    target_directory = input("Enter the directory path to organize: ").strip()
+    organize_files(target_directory)
