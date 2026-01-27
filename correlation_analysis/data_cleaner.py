fa956b5f969edcb74b45b7b627120e1e86564a92
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-27 23:38:50 +0000

    Add a function to remove outliers using the IQR method

--- correlation_analysis/data_cleaner.py
+++ correlation_analysis/data_cleaner.py
@@ -577,3 +577,24 @@ def clean_dataset(df, remove_outliers=True, normalize=True):
         cleaner.normalize_data(method='minmax')
     
     return cleaner.get_cleaned_data(), cleaner.get_cleaning_report()
+import numpy as np
+
+def remove_outliers_iqr(data, column):
+    """
+    Remove outliers from a specified column using the IQR method.
+    
+    Parameters:
+    data (pd.DataFrame): The input DataFrame.
+    column (str): The column name to process.
+    
+    Returns:
+    pd.DataFrame: DataFrame with outliers removed.
+    """
+    Q1 = data[column].quantile(0.25)
+    Q3 = data[column].quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    
+    filtered_data = data[(data[column] >= lower_bound) & (data[column] <= upper_bound)]
+    return filtered_data
