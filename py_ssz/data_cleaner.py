12f06016a455572be775e449561a581383b261cd
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-08 21:27:25 +0000

    Add a function to remove outliers using the IQR method

--- py_ssz/data_cleaner.py
+++ py_ssz/data_cleaner.py
@@ -718,3 +718,17 @@ def validate_data(data, column, min_value=None, max_value=None):
     validation_report['invalid_rows'] = (~mask).sum()
     
     return validation_report, data[mask]
+import numpy as np
+
+def remove_outliers_iqr(data, column):
+    """
+    Remove outliers from a specified column using the IQR method.
+    Returns a filtered DataFrame.
+    """
+    Q1 = data[column].quantile(0.25)
+    Q3 = data[column].quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    filtered_data = data[(data[column] >= lower_bound) & (data[column] <= upper_bound)]
+    return filtered_data
