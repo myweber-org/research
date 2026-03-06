bd97406c1039d66cfcdfa99b0ea4b7cf8af7c355
FelixMakes <felix.makes@hotmail.com>
2026-03-06 22:44:35 +0000

    Add data cleaning utility with outlier removal and normalization functions

--- mimc_stark/data_cleaner.py
+++ mimc_stark/data_cleaner.py
@@ -910,4 +910,40 @@ def clean_dataset(data, numeric_columns=None, outlier_multiplier=1.5):
 def remove_duplicates(seq):
     seen = set()
     seen_add = seen.add
-    return [x for x in seq if not (x in seen or seen_add(x))]
+    return [x for x in seq if not (x in seen or seen_add(x))]import numpy as np
+import pandas as pd
+from scipy import stats
+
+def remove_outliers_iqr(df, column):
+    Q1 = df[column].quantile(0.25)
+    Q3 = df[column].quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    return df[(df[column] >= lower_bound) & (df[column] <= upper_bound)]
+
+def normalize_minmax(df, column):
+    min_val = df[column].min()
+    max_val = df[column].max()
+    df[column + '_normalized'] = (df[column] - min_val) / (max_val - min_val)
+    return df
+
+def standardize_zscore(df, column):
+    mean_val = df[column].mean()
+    std_val = df[column].std()
+    df[column + '_standardized'] = (df[column] - mean_val) / std_val
+    return df
+
+def handle_missing_mean(df, column):
+    mean_val = df[column].mean()
+    df[column] = df[column].fillna(mean_val)
+    return df
+
+def clean_dataset(df, numeric_columns):
+    cleaned_df = df.copy()
+    for col in numeric_columns:
+        if col in cleaned_df.columns:
+            cleaned_df = remove_outliers_iqr(cleaned_df, col)
+            cleaned_df = handle_missing_mean(cleaned_df, col)
+            cleaned_df = normalize_minmax(cleaned_df, col)
+    return cleaned_df
