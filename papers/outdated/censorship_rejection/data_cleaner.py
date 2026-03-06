a3abf590e67a85489e73cdfa4d41db799f3216c9
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-03-06 23:28:11 +0000

    Add a function to remove outliers using the IQR method

--- papers/outdated/censorship_rejection/data_cleaner.py
+++ papers/outdated/censorship_rejection/data_cleaner.py
@@ -105,3 +105,13 @@ def clean_dataset(dataframe, outlier_columns=None, normalize=True, skew_threshol
     skewed_info = detect_skewed_columns(cleaned_df, skew_threshold)
     
     return cleaned_df, skewed_info
+import numpy as np
+
+def remove_outliers_iqr(data, column):
+    Q1 = data[column].quantile(0.25)
+    Q3 = data[column].quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    filtered_data = data[(data[column] >= lower_bound) & (data[column] <= upper_bound)]
+    return filtered_data
